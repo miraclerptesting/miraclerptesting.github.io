1a9838b07ab3e7168e03a8b62777fa5947716554
miraclerptesting <execuriverp@gmail.com>
2026-02-27 02:57:42 +0700

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD5JGQ3eARM5Lo2IG3mkpdx7p3uh0nrBIk",
+  authDomain: "samp-miracle.firebaseapp.com",
+  projectId: "samp-miracle",
+  storageBucket: "samp-miracle.firebasestorage.app",
+  messagingSenderId: "465126706451",
+  appId: "1:465126706451:web:b94f2359a348ff7078dee6",
+  measurementId: "G-ZCRVX5DCPR",
+  databaseURL: "https://samp-miracle-default-rtdb.asia-southeast1.firebasedatabase.app"  // ← TAMBAH INI
+};
